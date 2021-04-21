bc8699bc2442b5e147a949a4dad1c3c6f5890c78
Ryan Pinheior <gabrielryan024@gmail.com>
2021-04-21 00:27:51 -0300

    Programando o joystick

---
+++ Script/Analog.gd
@@ -0,0 +1,5 @@
+extends CanvasLayer
+
+var stick_speed = 0
+var stick_anlog = 0
+var stick_vecto = Vector2()
---
+++ Script/large .gd
@@ -0,0 +1,20 @@
+extends Sprite
+
+
+const RADIUS = 75
+const SMALL_RADIUS = 35
+var stik_pos
+var stik_controll = -1
+
+# Called when the node enters the scene tree for the first time.
+func _init():
+	
+	stik_pos = position
+
+func _input(event):
+	   if event is InputEventScreenTouch:
+		if event.is_
+	   
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
